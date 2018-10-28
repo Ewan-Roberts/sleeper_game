ee15aacff998dc7c1145729ff81eb0f82baa0132
Ewan <ewan@ceras.io>
2018-10-28 10:37:18 +0000

    :white_check_mark: removed cruft from pathfinding utils

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -2153,14 +2153,8 @@ module.exports.load_network_sprite = () => {
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"pixi.js":251}],20:[function(require,module,exports){
 (function (global){
-  const PIXI = require('pixi.js');
-// // const spriteHelper = require('../utils/spriteHelper.js');
-// const pathfinderData = require('./bedroom_grid8.json')
-
+const PIXI = require('pixi.js');
 const easystarjs = require('easystarjs');
-const easystar = new easystarjs.js();
-
-// // console.log(pathfinderData.layers[1].objects);
 
 global.grid_container = new PIXI.Container();
 global.grid_container.name = 'enemy_container';
@@ -2172,8 +2166,6 @@ module.exports.lay_down_grid = () => {
 
   const slantedWall = PIXI.Sprite.fromFrame('black_dot');
   slantedWall.name = 'slanted wall';
-  // slantedWall.width = 300;
-  // slantedWall.height = 300;
   slantedWall.position.set(0, 0);
   slantedWall.alpha = 0
   global.viewport.addChild(slantedWall);
@@ -2206,71 +2198,6 @@ module.exports.lay_down_grid = () => {
   global.viewport.addChild(global.grid_container);
 
 }
-
-// const renderTile = (array) => {
-
-//   array.forEach((elem) => {
-//     const tile = PIXI.Sprite.fromFrame('black_dot');
-//     tile.height = elem.height;
-//     tile.width = elem.width;
-//     tile.position.x = elem.x;
-//     tile.position.y = elem.y;
-//     global.viewport.addChild(tile);
-//   });
-// };
-
-// renderTile(pathfinderData.layers[1].objects);
-
-// const makeTwoDimentional = (array) => {
-//   array.sort((a, b) => a.id - b.id);
-//   for (let i = 0; i < array.length; i++) {
-//     if(array[i].type === 'walk') {
-//       array[i] = 0;
-//     } else {
-//       array[i] = 1;
-//     }
-//   }
-//   const newArr = [];
-//   while (array.length) newArr.push(array.splice(0, 55));
-//   return newArr;
-// }
-
-// const thingArray = makeTwoDimentional(pathfinderData.layers[1].objects);
-
-// const grid = [
-//   [0,0,1,0,0],
-//   [0,0,1,0,0],
-//   [0,0,1,0,0],
-//   [0,0,1,0,0],
-//   [0,0,0,0,0],
-// ];
-
-// console.log(grid);
-// console.log(thingArray);
-
-// easystar.setGrid(thingArray);
-// easystar.setAcceptableTiles([0]);
-// easystar.setIterationsPerCalculation(1000);
-
-// easystar.findPath(5, 5, 54, 17, function( path ) {
-//   if (path === null) {
-// 		console.log("Path was not found.");
-// 	} else {
-//     console.log(path);
-//     path.forEach((elem)=>{
-      
-//       const mover = PIXI.Sprite.fromFrame('black_dot');
-//       mover.width =20;
-//       mover.height =20;
-      
-//       mover.position.set(elem.x*10, elem.y*10);
-//       global.viewport.addChild(mover);
-//     })
-// 	}
-// });
-// setInterval(()=>{
-//   easystar.calculate();
-// },1000)
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"easystarjs":69,"pixi.js":251}],21:[function(require,module,exports){
