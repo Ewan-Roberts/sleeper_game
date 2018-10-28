bad2029a494d71d5509d0372afd626cfdad82549
Ewan <ewan@ceras.io>
2018-10-28 13:32:09 +0000

    :cherry_blossom: made server.js easier to read, removed one liners

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const port = process.env.PORT || 3000;
 const io = require('socket.io')(server, {});
 
 app.use(express.static('./public'));
-app.get('/', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
-server.listen(port, () => console.log(`server is listening on port: ${port}`));
+app.get('/', (req, res) => {
+  
+  res.sendFile(`${__dirname}/public/index.html`)
+  
+});
+
+server.listen(port, () => {
+  console.log(`server is listening on port: ${port}`)
+});
 
 io.on('connection', (socket) => {
   console.log('Client connected.');
