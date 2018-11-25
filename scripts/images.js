44d4475158c3e837d0897f27c7c5517b5e89f16b
Ewan <ewan@ceras.io>
2018-11-25 21:16:36 +0000

    :memo: Tidy comments from scripts

--- scripts/images.js
+++ scripts/images.js
@@ -1,28 +1,12 @@
-/* eslint-env node */
 const pngQuant = require('imagemin-pngquant');
-// const jpegTran = require('imagemin-jpegtran');
 
 module.exports = {
-  /**
-     * This defines a set of scales. For every scale a full set of
-     * spritesheets will be generated. The "resolution" field is passed
-     * through to PIXI.
-     * */
   scales: {
     web: { scale: 0.5, resolution: 1 },
   },
 
-  /**
-     * Variations can be used for themes or languages. Sprites that are
-     * not part of one variation will be included in all of them.
-     * */
   variations: ['EN'],
 
-  /**
-     * Different loading stages mean the game can started before all
-     * images have been loaded. Remaining images can be loaded while
-     * the user makes decisions or the game is going on.
-     * */
   loading_stages: ['bedroom'],
 
   group_default: {
@@ -33,18 +17,7 @@ module.exports = {
     padding: 0, // default: 1
   },
 
-  /**
-     * Groups are units of images that fall into the same category in respect to
-     * - Variations: EN, DE, or (if not defined) both. If an array is provided
-     *               the group will be added to all of the listed variations
-     * - JPEG: true/false (e.g. do we need an alpha channel?)
-     * - loading stage (see above)
-     * - quality
-     *
-     * All paths are relative to this file
-     * */
   groups: [
-
     {
       id: 'level_bedroom',
       variation: 'EN',
