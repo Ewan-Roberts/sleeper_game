d2f26fd20b74c45c983ac3cbc670b07d87cfcfa0
Ewan <ewan@ceras.io>
2018-12-09 12:01:11 +0000

    :art: Resolve linting

--- models/Item.js
+++ models/Item.js
@@ -1,43 +1,45 @@
+'use strict';
+
 const mongoose = require('mongoose');
 
 
-const ItemSchema = new mongoose.Schema({ 
-    
-    item_id: {
-        type:       Number,
-        required:   true
-    },
-    img: {
-        type:       String,
-        required:   true
-    },
-    name: {
-        type:       String,
-        required:   true
-    },
-    rarity: {
-        type:       String,
-        required:   true
-    },
-    drop_rate: {
-        type:       Number,
-        required:   true
-    },
-    materials: {
-        type:       Array,
-    },
-    mod: {
-        type:       Boolean
-    }
+const ItemSchema = new mongoose.Schema({
+
+  item_id: {
+    type:       Number,
+    required:   true,
+  },
+  img: {
+    type:       String,
+    required:   true,
+  },
+  name: {
+    type:       String,
+    required:   true,
+  },
+  rarity: {
+    type:       String,
+    required:   true,
+  },
+  drop_rate: {
+    type:       Number,
+    required:   true,
+  },
+  materials: {
+    type:       Array,
+  },
+  mod: {
+    type:       Boolean,
+  },
 
 },
 {
-    saveUnknown:            true,
-    timestamps: {
-        createdAt:          'creation_date',
-        updatedAt:          'last_update_date'
-    }
+  saveUnknown:            true,
+  timestamps: {
+    createdAt:          'creation_date',
+    updatedAt:          'last_update_date',
+  },
 });
 
 
-module.exports = mongoose.model("Item", ItemSchema)
+module.exports = mongoose.model('Item', ItemSchema);
