f2761925f23de686e82a25a937e2af3cf2cd3a75
Ewan <ewan@ceras.io>
2018-12-29 23:44:42 +0000

    :white_check_mark: Revert test

--- front_end/items/note.js
+++ front_end/items/note.js
@@ -23,7 +23,6 @@ class Note extends Item {
     this.dom_note = global.document.querySelector('.note');
     this.dom_note.addEventListener('click', () => {
       this.hide();
-      //test
     });
   }
 
