97a35bb4c3ee73279e6bec748be227cfb6f60e2e
Ewan <ewan@ceras.io>
2019-02-17 16:54:46 +0000

    :cherry_blossom: Remove viewport library

--- front_end/engine/shadows.js
+++ front_end/engine/shadows.js
@@ -8,16 +8,18 @@ require('pixi-shadows');
 
 const app = require('./app');
 
-const world = PIXI.shadows.init(app);
 //FOR TESTING
+const shadow = new PIXI.shadows.Shadow(900, 1);
+shadow.pointCount = 1;
+shadow.overlayLightLength = 1000;
+shadow.intensity = 3;
+shadow.ambientLight= 1;
+shadow.position.set(450, 150);
+
+const world = PIXI.shadows.init(app);
 PIXI.shadows.filter.ambientLight = 1;
 
-// Create a light that casts shadows
-// const shadow = new PIXI.shadows.Shadow(700, 1);
-// shadow.position.set(450, 150);
-// world.addChild(shadow);
-
-
+world.interactive = true;
 world.updateLayersOrder = function () {
   world.children.sort(function(a,b) {
     a.zIndex = a.zIndex || 0;
@@ -26,49 +28,7 @@ world.updateLayersOrder = function () {
   });
 };
 
-world.interactive = true;
-
-const shadow = new PIXI.shadows.Shadow(900, 1);
-shadow.pointCount = 1;
-shadow.overlayLightLength = 1000;
-shadow.intensity = 3;
-shadow.ambientLight= 1;
-shadow.position.set(450, 150);
-
 world.addChild(shadow);
-
-
-// Create some shadow casting demons
-// const demonTexture = PIXI.Texture.fromImage('bunny');
-// demonTexture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST; //For pixelated scaling
-
-// const demon1 = createShadowSprite(demonTexture, demonTexture);
-// demon1.position.set(100, 100);
-// demon1.scale.set(3);
-// world.addChild(demon1);
-
-// const demon2 = createShadowSprite(demonTexture, demonTexture);
-// demon2.position.set(500, 100);
-// demon2.scale.set(3);
-// world.addChild(demon2);
-
-// const demon3 = createShadowSprite(demonTexture, demonTexture);
-// demon3.position.set(300, 200);
-// demon3.scale.set(3);
-// world.addChild(demon3);
-
-// // Make the light track your mouse
-// world.interactive = true;
-// world.on("mousemove", function(event) {
-//   shadow.position.copy(event.data.global);
-// });
-
-// // Create a light point on click
-// world.on("pointerdown", function(event) {
-//   var shadow = new PIXI.shadows.Shadow(700, 0.7);
-//   shadow.position.copy(event.data.global);
-//   world.addChild(shadow);
-// });
 
 module.exports = {
   world,
