ada737e188e64595f04df286483ffd212ec4a112
Ewan <ewan@ceras.io>
2019-03-04 17:31:59 +0000

    :tada: Add Spine animation (in process of replacing spritesheet)

--- web/index.js
+++ web/index.js
@@ -1,14 +1,33 @@
 'use strict';
+const PIXI = require('pixi.js');
 
 require('./engine/pixi_containers');
-require('./engine/shadows');
+require('pixi-spine');
+require('./engine/globals');
+
+const { world } = require('./engine/shadows');
+
 
 const { loader } = require('./engine/packer');
 loader.add('../../images/bedroom_EN_web.json');
+loader.add('spineCharacter', 'images/Dragon.json');
+loader.load(async function(loader, resources) {
+  const animation = new PIXI.spine.Spine(resources.spineCharacter.spineData);
+  animation.height = 100;
+  animation.width = 100;
+  animation.x = 1000;
+  animation.y = 400;
+  world.addChild(animation);
 
-loader.load(async function() {
+  const animation1 = new PIXI.spine.Spine(animation.spineData);
+  if (animation1.state.hasAnimation('walk')) {
+    // run forever, little boy!
+    animation.state.setAnimation(0, 'walk', true);
+    // dont run too fast
+    animation.state.timeScale = 0.1;
+  }
+
   // ONLY FOR TESTING
-  require('./engine/globals');
 
   const { DevelopmentLevel } = require('./level/dev_level.js');
 
