304f25129a00475b4a65d31d898bce3fc2524cd1
ewan-portable <jojothegreat694@hotmail.com>
2019-07-01 19:11:58 +0100

    :bud: Fix item going to wrong container

--- web/level/elements/chest.js
+++ web/level/elements/chest.js
@@ -8,7 +8,6 @@ const { Note      } = require('../../view/overlay_object');
 const { Caption   } = require('../../view/caption');
 const { Sprite, Texture } = require('pixi.js');
 
-
 class Chest extends Sprite {
   constructor(data) {
     const { properties } = data;
@@ -19,11 +18,13 @@ class Chest extends Sprite {
     this.rotation = data.rotation * (Math.PI/180);
     this.alpha    = properties && properties.alpha || 1;
     this.anchor.set(0, 1);
+
     this.position.copy(data);
     this.interactive = true;
-    if(data.type === 'note') this.on('click', () => new Note(properties));
-    if(properties.collision) collisions.addChild(this);
-    items.addChild(this);
+
+    if(data.type === 'note') {
+      this.on('click', () => new Note(properties))
+    };
 
     if(properties.equip_on_click) {
       this.click = () => {
@@ -33,6 +34,7 @@ class Chest extends Sprite {
     }
 
     if(properties.label) this.label(properties);
+
     if(properties.dialog_on_click) this.on('click', () => {
       Caption.render(properties.dialog_on_click);
     });
@@ -40,12 +42,21 @@ class Chest extends Sprite {
     if(properties.remove_on_click) this.on('click', () => {
       this.destroy();
     });
-    if(properties.container) this.container(properties);
+
+    if(properties.container) {
+      this.container(properties);
+    }
+
+    if(properties.collision) {
+      collisions.addChild(this);
+    }
+
+    items.addChild(this);
   }
 
   container(properties) {
     this.inventory = new Inventory(properties);
-    this.click = () => {
+    this.on('click', () => {
       this.button.visible = false;
       this.inventory.set_position(this);
       this.inventory.fade_in();
@@ -64,7 +75,7 @@ class Chest extends Sprite {
       this.tint = 0xd3d3d3;
       this.button.visible = false;
     });
-    this.click = () => {
+    this.on('click', () => {
       this.tint = 0xd3d3d3;
       this.button.visible = false;
     };
@@ -73,10 +84,6 @@ class Chest extends Sprite {
   remove() {
     this.button.destroy();
     this.destroy();
-  }
-
-  fill() {
-    this.state = 'full';
   }
 }
 
